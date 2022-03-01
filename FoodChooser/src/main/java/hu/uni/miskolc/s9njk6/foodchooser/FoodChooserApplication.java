375c14177c9b4ff756e63c6957b320cfafdb8f63
Gabszi2 <gabszi12345@gmail.com>
2022-03-01 17:31:22 +0100

    setup

---
+++ FoodChooser/src/main/java/hu/uni/miskolc/s9njk6/foodchooser/FoodChooserApplication.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.s9njk6.foodchooser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodChooserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodChooserApplication.class, args);
+    }
+
+}
---
+++ FoodChooser/src/test/java/hu/uni/miskolc/s9njk6/foodchooser/FoodChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.s9njk6.foodchooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodChooserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
