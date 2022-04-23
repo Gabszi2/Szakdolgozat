7180a320c01a7e9b5953b983f3e969f6572ca10b
Gabszi2 <gabszi12345@gmail.com>
2022-04-23 10:58:49 +0200

    question modify done

--- FoodChooser/src/main/angular/src/app/admin-question-components/admin-question-modify/admin-question-modify.component.ts
+++ FoodChooser/src/main/angular/src/app/admin-question-components/admin-question-modify/admin-question-modify.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AdminQuestionService} from "../../services/admin-question.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {QuestionSaveModel} from "../../models/question-save-model";
 
 @Component({
   selector: 'app-admin-question-modify',
@@ -6,11 +10,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./admin-question-modify.component.css']
 })
 export class AdminQuestionModifyComponent implements OnInit {
-
-  constructor() {
+  kitchen!:string;
+  oldQuestion!:string;
+  questionForm!:FormGroup;
+  constructor(private service:AdminQuestionService,private route: ActivatedRoute,private router:Router,private formBuilder:FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.kitchen=<string>this.route.snapshot.paramMap.get('kitchen');
+    this.oldQuestion=<string>this.route.snapshot.paramMap.get('question')
+    this.questionForm=this.formBuilder.group({
+      question:['',Validators.required]
+    })
   }
-
+  async questionModify(){
+    const newQuestion=this.questionForm.get('question')?.value;
+    const questionUpdate=<QuestionSaveModel>{};
+    questionUpdate.oldQuestion=this.oldQuestion;
+    questionUpdate.newQuestion=newQuestion;
+    await this.service.updateQuestion(this.kitchen,questionUpdate);
+    await this.router.navigate(['/admin/question-list/' + this.kitchen])
+  }
 }
