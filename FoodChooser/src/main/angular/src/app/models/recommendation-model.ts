bf2150e420ae0aa7eaafefddc49999b75f2a6911
Gabszi2 <gabszi12345@gmail.com>
2023-02-03 15:33:38 +0100

    services implement

---
+++ FoodChooser/src/main/angular/src/app/models/recommendation-model.ts
@@ -0,0 +1,10 @@
+export interface RecommendationModel{
+  id:number;
+  type:string;
+  kitchen:string;
+  city:string;
+  restaurant:string;
+  message:string;
+  approved:boolean;
+
+}
--- FoodChooser/src/main/angular/src/app/services/customer.service.ts
+++ FoodChooser/src/main/angular/src/app/services/customer.service.ts
@@ -2,19 +2,20 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {lastValueFrom} from "rxjs";
 import {FoodModel} from "../models/food-model";
+import {RecommendationModel} from "../models/recommendation-model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-  adminUrl: string;
+  url: string;
 
   constructor(private http: HttpClient) {
-    this.adminUrl = 'http://localhost:8080/'
+    this.url = 'http://localhost:8080/'
   }
 
   async getQuestions(kitchen: string) {
-    return lastValueFrom(this.http.get<string[]>(this.adminUrl + 'customer-questions/' + kitchen));
+    return lastValueFrom(this.http.get<string[]>(this.url + 'customer-questions/' + kitchen));
   }
 
   async getResult(answers: boolean[], town: string, kitchen: string) {
@@ -23,6 +24,9 @@ export class CustomerService {
       httpParams = httpParams.append('answers', answer);
     })
     let options = {params: httpParams};
-    return lastValueFrom(this.http.get<FoodModel>(this.adminUrl + 'result/' + town + '/' + kitchen, options));
+    return lastValueFrom(this.http.get<FoodModel>(this.url + 'result/' + town + '/' + kitchen, options));
+  }
+  async createRecommendation(recommendation:RecommendationModel){
+    return lastValueFrom(this.http.post<RecommendationModel>(this.url+'/customer-recommendation',recommendation));
   }
 }
